393f6a6f1d0af70d413c290770092c2043c209eb
Kyle Goode <kyledgoode15@gmail.com>
2026-02-01 21:37:14 -0600

    added unit tests for numberProcessor.js

--- src/numberProcessor.js
+++ src/numberProcessor.js
@@ -1,41 +1,56 @@
+function putNumbersInArray(numbers) {
+    let array = [];
+    const sameLineText = numbers.split("\n");
+    for (let i = 0; i < sameLineText.length; i++) {
+        const temp = sameLineText[i].trim();
+        array.push(Number(temp));
+    }
+    return array;
+}
+
 function findMax(numbers) {
-    let max = numbers[0];
-    for (let i = 1; i < numbers.length; i++) {
-        if (numbers[i] > max) {
-            max = numbers[i];
+    const array = putNumbersInArray(numbers);
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
         }
     }
     return max;
 }
 
 function findMin(numbers) {
-    let min = numbers[0];
-    for (let i = 1; i < numbers.length; i++) {
-        if (numbers[i] < min) {
-            min = numbers[i];
+    const array = putNumbersInArray(numbers);
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
         }
     }
     return min;
 }
 
 function findSum(numbers) {
+    const array = putNumbersInArray(numbers);
     let sum = 0;
-    for (const num of numbers) {
+    for (const num of array) {
         sum += num;
     }
     return sum;
 }
 
 function findAverage(numbers) {
+    let array = putNumbersInArray(numbers);
     let sum = findSum(numbers);
-    if (numbers.length === 0) {
+    if (array.length === 0) {
         return 0;
     }
-    let average = sum / numbers.length;
+    let average = sum / array.length;
     return average;
 }
 
 module.exports = {
+    putNumbersInArray,
     findMax,
     findMin,
     findSum,
---
+++ tests/numberProcessor.test.js
@@ -0,0 +1,75 @@
+const{
+    putNumbersInArray,
+    findMax,
+    findMin,
+    findSum,
+    findAverage
+} = require('../src/numberProcessor');
+
+describe('Number Processor Functions', () => {
+    describe('putNumbersInArray', () => {
+        test('converts string of numbers into array', () => {
+            const numbers = "1\n2\n3\n4";
+            expect(putNumbersInArray(numbers)).toEqual([1,2,3,4]);
+        });
+    });
+    describe('findMax', () => {
+        test('finds max from positive numbers', () => {
+            const numbers = [1, 9, 3, 6]
+            expect(findMax(numbers)).toBe(9);
+        });
+        test('finds max from negative numbers', () => {
+            const numbers = [-1, -9, -3, -6]
+            expect(findMax(numbers)).toBe(-1);
+        });
+        test('finds max from null', () => {
+            const numbers = []
+            expect(findMax(numbers)).toBe(0);
+        });
+    });
+
+    describe('findMin', () => {
+        test('finds min from positive numbers', () => {
+            const numbers = [1, 9, 3, 6]
+            expect(findMin(numbers)).toBe(1);
+        });
+        test('finds min from negative numbers', () => {
+            const numbers = [-1, -9, -3, -6]
+            expect(findMin(numbers)).toBe(-9);
+        });
+        test('finds min from null', () => {
+            const numbers = []
+            expect(findMin(numbers)).toBe(0);
+        });
+    });
+
+    describe('findSum', () => {
+        test('finds sum from positive numbers', () => {
+            const numbers = [1, 9, 3, 6]
+            expect(findSum(numbers)).toBe(19);
+        });
+        test('finds sum from negative numbers', () => {
+            const numbers = [-1, -9, -3, -6]
+            expect(findSum(numbers)).toBe(-19);
+        });
+        test('finds sum from null', () => {
+            const numbers = []
+            expect(findSum(numbers)).toBe(0);
+        });
+    });
+
+    describe('findAverage', () => {
+        test('finds average from positive numbers', () => {
+            const numbers = [1, 9, 3, 6]
+            expect(findAverage(numbers)).toBe(5.25);
+        });
+        test('finds average from negative numbers', () => {
+            const numbers = [-1, -9, -3, -6]
+            expect(findAverage(numbers)).toBe(-5.25);
+        });
+        test('finds average from null', () => {
+            const numbers = []
+            expect(findAverage(numbers)).toBe(0);
+        });
+    });
+});
